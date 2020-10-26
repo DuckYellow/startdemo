ed81512fc59310eb68ae59a61a69c08fd00945b2
xuweihang <xuweihang@qbb6.com>
2020-10-27 00:13:58 +0800

    dubbo

--- src/main/java/com/example/startdemo/leetcode/Solution2.java
+++ src/main/java/com/example/startdemo/leetcode/Solution2.java
@@ -560,16 +560,20 @@ public class Solution2 {
 
 
     public static void main(String[] args) {
-        int k = 3;
-        int[] arr = new int[]{4, 5, 8, 2};
-        KthLargest kthLargest = new KthLargest(3, arr);
 
+        System.out.println(18002130 % 8);
+        System.out.println(18002130 % 512);
+        System.out.println(9244746 % 8);
+        System.out.println(9244746 % 512);
 
-        System.out.println(kthLargest.add(3));
-        System.out.println(kthLargest.add(5));
-        System.out.println(kthLargest.add(10));
-        System.out.println(kthLargest.add(9));
-        System.out.println(kthLargest.add(4));
+        System.out.println(18002458 % 8);
+        System.out.println(18002458 % 512);
+
+        System.out.println(18002298 % 8);
+        System.out.println(18002298 % 512);
+
+        System.out.println(2375 % 8);
+        System.out.println(2375 % 512);
     }
 
 
